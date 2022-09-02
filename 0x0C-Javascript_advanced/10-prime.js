d5688752767a3699a57990aea2768a12eed030fd
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-02 21:21:53 +0200

    Execution stack & timing execution

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,22 @@
+function countPrimeNumbers() {
+    let counter = 0;
+    for (let i = 2; i <= 100; i++) {
+        let flag = true;
+
+        for(let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                flag = false;
+            }
+        }
+
+        if (flag) {
+            counter +=1;
+        }
+    }
+    return counter + 1;
+}
+let starttime = performance.now();
+for ( i = 0; i < 100; ++i) {
+    countPrimeNumbers();
+}
+console.log("Execution time of printing countPrimeNumbers was " + (starttime - performance.now()) +  " milliseconds.");
